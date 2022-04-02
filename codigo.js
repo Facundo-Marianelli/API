761e2f2145c2778b6a7f2ffcbfddfcf1d12645e5
Facundo-Marianelli <facumarianelli@gmail.com>
2022-04-01 22:59:17 -0300

    Intial Commit

---
+++ codigo.js
@@ -0,0 +1,24 @@
+//Probando Map
+var a = ["Pedro", "Facundo"];
+var a2= a.map(function(s){return s.length})
+var a3= a.map(s => s.length);
+console.log(a2);
+console.log("Map con flecha "+a3);
+
+//Declarando objetos
+
+var miAuto = new Object();
+miAuto.marca = "Ford";
+miAuto.modelo = "Focus";
+miAuto.año = "2020";
+console.log(miAuto.marca);
+
+//Declarando funciones:
+function Auto(marca, modelo, año)
+{
+    this.marca=marca;
+    this.modelo=modelo;
+    this.año=año;
+}
+var miCarro=new Auto("Renautl", "Sandero", "2012");
+console.log(miCarro);
